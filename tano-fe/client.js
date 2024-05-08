4f85a38cd1fd9fad0c771ae8af79455201c2b242
loc_nguyenpb <nguyenphucbuuloc.cdn@gmail.com>
2024-05-08 21:30:23 +0700

    update cache data

--- tano-fe/client.js
+++ tano-fe/client.js
@@ -1,9 +1,21 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
     link: new HttpLink({
       uri: `${process.env.NEXT_PUBLIC_DB_HOST}/graphql`,
     }),
